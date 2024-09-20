da1379031494954c5ad8a8bb41cf540adee8127a
terabox25 <164458249+terabox25@users.noreply.github.com>
2024-09-20 18:42:31 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -31,7 +31,7 @@ async def help_user(bot, update):
         text=Translation.HELP_USER,
         parse_mode="html",
         disable_web_page_preview=True,
-        reply_to_message_id=update.message_id
+        reply_to_message_id=update.id
     )
 
 
