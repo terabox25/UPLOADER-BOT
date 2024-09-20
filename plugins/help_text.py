fce85fc484a8e9ca00b3d83f3f965fc003a207d3
terabox25 <164458249+terabox25@users.noreply.github.com>
2024-09-20 18:57:13 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -29,7 +29,7 @@ async def help_user(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_USER,
-        parse_mode="html",
+        parse_mode="markdown",
         disable_web_page_preview=True,
         reply_to_message_id=update.id
     )
